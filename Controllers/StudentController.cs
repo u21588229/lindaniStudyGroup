f4a611fbaf64581547ff07847a38feb345815098
Lindani <Lindani@LAPTOP-EGD9HVG2>
2025-03-03 18:18:17 +0200

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lindaniStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Models.Student> student = new List<Models.Student>();
+
+            student.Add(new Models.Student { StudentNumber = "u21588229", Name = "Lindani", Surname = "Zwane", Email = "lindani.zwane@tuks.com"});
+            student.Add(new Models.Student { StudentNumber = "u21675947", Name = "Bob", Surname = "Dlamini", Email = "john.dlamini@tuks.com" });
+            student.Add(new Models.Student { StudentNumber = "u21759406", Name = "Okuhle", Surname = "Mavuso", Email = "okuhle.mavuso@tuks.com" });
+            student.Add(new Models.Student { StudentNumber = "u21864848", Name = "Daniel", Surname = "Cardoso", Email = "daniel.cardoso@tuks.com" });
+            student.Add(new Models.Student { StudentNumber = "u21956302", Name = "Rodrick", Surname = "Hwaire", Email = "rodrick.Hwaire@tuks.com" });
+
+            return View(student);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lindaniStudyGroup.Models
+{
+	public class Student
+	{
+
+		[Display(Name = "STUDENT NUMBER")]
+		public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")]
+        public string Name { get; set; }
+
+        [Display(Name = "SURNAME")]
+        public string Surname { get; set; }
+
+        [Display(Name = "EMAIL")]
+        public string Email { get; set; }
+
+    }
+}
